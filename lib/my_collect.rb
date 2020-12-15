65d2a05438c782640b57037ff56af132afae7ae0
Jonathan Montesdeoca <JMontesdeoca93@gmail.com>
2020-12-15 15:52:34 -0500

    commit tree daef3202d23817af2cfd8c04b6d2b0169a20dd5f

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(list)
+    if !list.empty?
+        i = 0
+         collection = []
+         while i < list.length
+            collection << yield(list[i])
+            i += 1
+         end
+         collection
+    end     
+end
 
 
